3df644c4c5fce640af3d6c849ce7062a3ef9af57
Akmal <madiyarovakmal219@gmail.com>
2023-06-25 14:00:50 +0500

    push responsve-modal

--- js/modal.js
+++ js/modal.js
@@ -1,6 +1,7 @@
 var modaBtn = document.querySelector('.modal-button')
 var modaBg = document.querySelector('.modal-bg')
 var modaClose = document.querySelector('.modal-close')
+var modaCloses = document.querySelector('.class-btn')
 
 modaBtn.addEventListener('click', function(){
     modaBg.classList.add('bg-active')
@@ -9,3 +10,8 @@ modaBtn.addEventListener('click', function(){
 modaClose.addEventListener('click', function(){
     modaBg.classList.remove('bg-active')
 })
+
+modaCloses.addEventListener('click', function(){
+    modaBg.classList.remove('bg-active')
+})
+
