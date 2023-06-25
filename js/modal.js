913f4516f961674db5d0a2c32ad496659ab99a8f
Akmal <madiyarovakmal219@gmail.com>
2023-06-25 11:05:57 +0500

    hazzat

---
+++ js/modal.js
@@ -0,0 +1,8 @@
+const modal = document.querySelector('.none')
+const btn = document.querySelector('.btn')
+
+
+btn.onclick = function(){
+    modal.classList.toggle('btn')
+    console.log("Hellow");
+}
